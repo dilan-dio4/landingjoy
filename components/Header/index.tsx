48dc0332c92b0290be7a8b132b54c934b964bd23
dilan <dio4@pitt.edu>
2022-07-23 14:23:57 -0400

    Get other font in

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,6 @@
-import Link from 'next/link';
 import clsx from 'clsx';
 import styles from '../../styles/components/Header.module.css';
-import { useEffect, useState, useRef, useContext, FC } from 'react';
+import React, { useEffect, useState, useRef, useContext, FC } from 'react';
 import useDeviceSize from '../../utils/useDeviceSize';
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 import UiContext from '../Context/UiContext';
@@ -10,7 +9,6 @@ import Tooltip from '../Tooltip';
 import { Lottie } from '@alfonmga/react-lottie-light-ts';
 import planetJson from '../../assets/lottie/planet-and-stars.json';
 import { FloatingProps } from '../Tooltip/Floating';
-import { ReactComponent as Logo2 } from '../../assets/logo/2.svg';
 
 export default function Header() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
@@ -82,15 +80,19 @@ export default function Header() {
         className: '-mb-[1px]',
     };
 
+    const logoStyle: React.CSSProperties = {
+        fontFamily: '"Lobster Two", "Ubuntu", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+        fontSize: 22,
+        maxHeight: 0,
+        marginLeft: -20,
+        lineHeight: 0
+    }
+
     return (
         <div className={''}>
             <div className={clsx('flex justify-center w-full', styles['nav-root'], 'top-0')} ref={headerRef}>
                 <div className='flex flex-row w-[1360px] max-w-full md:px-[48px] px-6'>
-                    <div className='flex basis-1/4 items-center justify-start'>
-                        <Link href='/'>
-                            <a>{/* <IconPos height={"35px"} className="ml-[-8px] cursor-pointer" /> */}</a>
-                        </Link>
-                    </div>
+                    <div className='flex basis-1/4' />
                     {isMobile ? (
                         <div className='flex basis-3/4 items-center justify-end'>
                             {/* <NavPrimaryButton text="Log In" className='!w-[100px] !mr-3' /> */}
@@ -101,17 +103,33 @@ export default function Header() {
                         </div>
                     ) : (
                         <>
-                            <nav className='flex basis-1/2 items-center justify-center'>
+                            <nav className='flex-center basis-2/4'>
                                 <ul
                                     itemScope
                                     itemType='https://schema.org/SiteNavigationElement'
                                     role='menu'
                                     className={clsx('list-none inline-block whitespace-nowrap', styles['nav-ul'])}
                                 >
-                                    {/* <li itemProp="name" role="menuitem" >
-                                        <Link href="https://docs.easybase.io/microstacks/what-is-a-microstack"><a itemProp="url" title="What is a Microstack?">What is a Microstack?</a></Link>
-                                    </li> */}
-                                    <li><Logo2 /></li>
+                                    <li itemProp='name' role='menuitem' className=''>
+                                        <a href='#' aria-disabled itemProp='url' title='Methodology' style={logoStyle} >
+                                            Landingjoy
+                                        </a>
+                                    </li>
+                                    <div className='inline-block align-middle mx-1'>
+                                        <svg
+                                            stroke="currentColor"
+                                            fill="none" 
+                                            strokeWidth="1"
+                                            viewBox="0 0 24 24"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            width="32px"
+                                            opacity="0.8"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 5v14"></path>
+                                        </svg>
+                                    </div>
                                     <li itemProp='name' role='menuitem'>
                                         <a href='#methodology' itemProp='url' title='Methodology'>
                                             Methodology
@@ -127,6 +145,11 @@ export default function Header() {
                                             FAQs
                                         </a>
                                     </li>
+                                    <li itemProp='name' role='menuitem'>
+                                        <a href='#contact-us' itemProp='url' title='Contact Us'>
+                                            Contact us
+                                        </a>
+                                    </li>
                                     <li className='!inline-flex align-bottom'>
                                         <Tooltip {...tooltipProps}>
                                             <DarkModeSwitch {...darkModeSwitchProps} />
@@ -134,9 +157,7 @@ export default function Header() {
                                     </li>
                                 </ul>
                             </nav>
-                            <div className='flex basis-1/4 items-center justify-end'>
-                                {/* <NavPrimaryButton text="Sign Up" href="https://app.easybase.io?view=signup" onClick={() => gtag('event', 'sign_up', {})} /> */}
-                            </div>
+                            <div className='flex basis-1/4' />
                         </>
                     )}
                 </div>

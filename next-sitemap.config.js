93deeb3c2bb2a7b84b073739b7781f9e1d5209c0
dilan <dio4@pitt.edu>
2022-07-24 10:39:56 -0400

    sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
-const config = {
+module.exports = {
     siteUrl: process.env.SITE_URL || 'https://landingjoy.com',
     generateRobotsTxt: true, // (optional)
     // ...other options
 }
-
-export default config
